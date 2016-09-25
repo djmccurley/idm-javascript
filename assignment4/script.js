80d328a5a515297fdc1ea9a978dd7c70539341a4
Dan McCurley <djmccurley@gmail.com>
2016-09-24 22:40:17 -0500

    time elapsed variable working correctly

--- assignment4/script.js
+++ assignment4/script.js
@@ -10,11 +10,16 @@ var thirdSpeech = new Speech("Demosthenes", "342 B.C.E.", true);
 
 var allSpeeches = [firstSpeech, secondSpeech, thirdSpeech];
 
+//There is probably a more elegant way to make this work, but I like this round about solution.
+var timePassed = 	parseInt(allSpeeches[1].year.substring(
+										allSpeeches[1].year.length-4, allSpeeches[1].year.length)) - 
+									parseInt(allSpeeches[0].year.substring(
+										allSpeeches[0].year.length-4, allSpeeches[0].year.length));
 
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
-  console.log(allSpeeches[1].name);
+  console.log(timePassed);
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
