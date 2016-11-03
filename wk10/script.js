c0d338cec07e25bed190485f44b9c490ac28a6b7
Dan McCurley <djmccurley@gmail.com>
2016-11-03 16:34:36 -0500

    checkboxes added

--- wk10/script.js
+++ wk10/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
-	//changes bg color based on data-type attribute
+	//changes bg color based on data-type attribute (assingment 10.1)
 	$("[data-type='vitamin']").css("background", "#3d9323");
 	$("[data-type='mineralwater']").css("background", "#42b6f4");
 	$("[data-type='proteinbar']").css("background", "#9b42f4");
+
+	//assignment 10.2 - hide/display products on checkbox selection
+
+	
 });
