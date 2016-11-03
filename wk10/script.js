ba18364d059198fdb82c03c0ec51bdb3dfe6be79
Dan McCurley <djmccurley@gmail.com>
2016-11-03 12:33:35 -0500

    assignment complete

---
+++ wk10/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	//changes bg color based on data-type attribute
+	$("[data-type='vitamin']").css("background", "#3d9323");
+	$("[data-type='mineralwater']").css("background", "#42b6f4");
+	$("[data-type='proteinbar']").css("background", "#9b42f4");
+});
