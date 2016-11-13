9db49bfe8289266b48dc3a06455c307834f66770
Dan McCurley <djmccurley@gmail.com>
2016-11-13 14:50:54 -0600

    h1 click changes color of header div

--- assignment11/script.js
+++ assignment11/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+	var mainHeader = $("#header");
+	//add attached icon to nav li
 	$("#menu a").prepend('<img src="icon.png">');
+	//click listener on h1 turns bg color of header div
+	$("#header h1").click(function() {
+		mainHeader.css('background-color', '#e31f32');
+	})
 });
