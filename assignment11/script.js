9e0521a59b1a01c78c9da469081c2f05324626cd
Dan McCurley <djmccurley@gmail.com>
2016-11-13 15:10:27 -0600

    added click listener to content h2s

--- assignment11/script.js
+++ assignment11/script.js
@@ -9,4 +9,10 @@ $(document).ready(function() {
 	//change color of first li (actually the a within that li) in sidebar 
 	$("#sidebar_content_2 li:first-of-type a").css("color", "red");
 	console.log('li color changed!');
+	//move even numbered LI elements 5px to the right (cheated and used padding-left)
+	$("#sidebar_content_1 li:odd").css("padding-left", "5px");
+	//replace content div on h2 clicks with a picture of Chuck Norris
+	$("#content h2").click(function() {
+		$("#content").html("<img src='chuckles.jpg'><h2>Great Job!</h2>");
+	});
 });
