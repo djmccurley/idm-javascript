0e9e3b6a65cd99891e98481cc18f79f3d8876471
Dan McCurley <djmccurley@gmail.com>
2016-11-13 14:55:43 -0600

    li color changed in second side menu

--- assignment11/script.js
+++ assignment11/script.js
@@ -5,5 +5,8 @@ $(document).ready(function() {
 	//click listener on h1 turns bg color of header div
 	$("#header h1").click(function() {
 		mainHeader.css('background-color', '#e31f32');
-	})
+	});
+	//change color of first li (actually the a within that li) in sidebar 
+	$("#sidebar_content_2 li:first-of-type a").css("color", "red");
+	console.log('li color changed!');
 });
