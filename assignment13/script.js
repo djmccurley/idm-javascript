6d25ec96b63300574c57101ea6153c34d0ca3808
Dan McCurley <djmccurley@gmail.com>
2016-11-29 22:41:11 -0600

    functionality 100%

--- assignment13/script.js
+++ assignment13/script.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
-  function makeBoxes(num) {
+  function makeBoxes(num, col, cost) {
     if(isNaN(num)){
-      alert("Please enter a valid number!");
-    } else {
-        $("#box-holder").html("");  
-        for(var i=1; i<=num; i++){
-          $("#box-holder").append("<div><p>" + i + "</p></div>");
-        }
-      }  
+      $("#box-holder").html("Please enter a valid number 1 - 1000");
+    } else if(num > 1000) {
+        $("#box-holder").html("Sorry, Box Union doesn't pay overtime! Orders must be 1000 boxes or less");
+      } 
+        //had to specify range here to avoid condition running
+        //on undefined value for quantity
+        else if(num >=1 && num <= 1000) { 
+          $("#box-holder").html("");  
+          for(var i=1; i<=num; i++){
+            $("#box-holder").append("<div><p>" + i + "</p></div>");
+          }
+          $("#box-holder").children().css("background-color", col);
+          $("#history").prepend("<p>" + num + " " + col + " boxes @ $" + cost + " each = $" + (num*cost) + "</p>");
+        }  
+
   }
 
   $("#start").click(function() {
-    var boxes = prompt("How many boxes do you want?");
-    makeBoxes(boxes);
+    var numberOfBoxes = $("#quantity").val();
+    var colorOfBoxes = $("#color").val();
+    var costOfBoxes = $("#cost").val();
+
+    makeBoxes(numberOfBoxes, colorOfBoxes, costOfBoxes);
+  });
+
+  $("#view-history").click(function(){
+    $("#history").slideToggle();
   });
 });  
