2a1be23f9839c2a3312197a9457252fc6dab214b
Dan McCurley <djmccurley@gmail.com>
2016-11-30 12:29:17 -0600

    added sliding functionality to boxes on each order

--- assignment13/script.js
+++ assignment13/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
   function makeBoxes(num, col, cost) {
     if(isNaN(num) || (!num)){
-      $("#box-holder").html("<h3>Please enter a valid number 1 - 1000</h3>");
+      $("#box-slider").html("<h3>Please enter a valid number 1 - 1000</h3>");
     } else if(num > 1000) {
-        $("#box-holder").html("<h3>Sorry, Box Maker Union doesn't pay overtime! Orders must be 1000 boxes or less</h3>");
+        $("#box-slider").html("<h3>Sorry, Box Maker Union doesn't pay overtime! Orders must be 1000 boxes or less</h3>");
       } 
         //had to use boolean here to avoid this chunk running on undefined value
         else if(num) { 
-          $("#box-holder").html("");  
+          $("#box-slider").html("").css("display", "none");  
           for(var i=1; i<=num; i++){
-            $("#box-holder").append("<div><p>" + i + "</p></div>");
+            $("#box-slider").append("<div class='box'><p>" + i + "</p></div>");
           }
-          $("#box-holder").children().css("background-color", col);
+          $("#box-slider").children().css("background-color", col);
+          $("#box-slider").slideDown();
           $("#history").prepend("<p>" + num + " " + col + " boxes @ $" + cost + " each = $" + (num*cost) + "</p>");
         }  
 
