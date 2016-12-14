5dafdc83e423d902600f2240564ba0b7feb03113
Dan McCurley <djmccurley@gmail.com>
2016-12-14 13:03:20 -0600

    encoding function works with fix to keep all letters in ascii range of 26 characters

--- final/script.js
+++ final/script.js
@@ -1,16 +1,51 @@
 	var outputString;
+	var shiftValue = 13;
 	document.getElementById("input").onkeyup = function(){
 		outputString = document.getElementById("input").value.toUpperCase();	  
 	  rot13(outputString);
 	};
 
-	function rot13(str) { // LBH QVQ VG!
-	  var decodedValues = [];
+	function rot13(str) { // ENCODER FUNCTION
+	  var encodedValues = [];
 	  var letterCode;
+	  var shiftedLetterCode;
 	  var decodedString = '';
 	  
-	// iterates through str and pushes converted utf code to array
-	  for(i=0; i<str.length; i++){
+		// iterates through str and pushes converted utf code to array
+		for(i=0; i<str.length; i++){
+	    letterCode = str.charCodeAt(i);
+	    if(letterCode <= 64 || letterCode >= 91) {
+	      encodedValues.push(letterCode);
+	    } else if (letterCode > 64 && letterCode < 91) {
+	      shiftedLetterCode = letterCode + shiftValue;
+      		if(shiftedLetterCode > 90) {
+						shiftedLetterCode = 64 + (shiftedLetterCode - 90);
+					}
+		    }
+		  encodedValues.push(shiftedLetterCode);  
+		} 
+
+
+
+	//logs resulting array to check accuracy  
+	  console.log(encodedValues);
+	  
+	//iterates through value of true utf codes - concatenates onto string  
+	  for(j=0; j<encodedValues.length; j++) {
+	    decodedString = decodedString + String.fromCharCode(encodedValues[j]);
+	  }
+	  
+	  console.log(decodedString);
+	  document.getElementById("output").innerHTML = decodedString;
+
+   
+  
+}
+
+
+/* ORIGINAL FUNCTION	  
+
+for(i=0; i<str.length; i++){
 	    letterCode = str.charCodeAt(i);
 	    if(letterCode <= 64) {
 	      decodedValues.push(letterCode);
@@ -20,17 +55,4 @@
 	      decodedValues.push(letterCode - 13);
 	    }
 	  }
-	//logs resulting array to check accuracy  
-	  console.log(decodedValues);
-	  
-	//iterates through value of true utf codes - concatenates onto string  
-	  for(j=0; j<decodedValues.length; j++) {
-	    decodedString = decodedString + String.fromCharCode(decodedValues[j]);
-	  }
-	  
-	  console.log(decodedString);
-	  document.getElementById("output").innerHTML = decodedString;
-
-   
-  
-}
+*/
