754932806929d15ae0b8dae2c2cd945f3d6a54dd
Dan McCurley <djmccurley@gmail.com>
2016-12-15 22:25:08 -0600

    mvp complete

--- final/script.js
+++ final/script.js
@@ -1,4 +1,10 @@
+// clipboard functionality from https://clipboardjs.com/
 var clipboard = new Clipboard('.copy');
+/*
+	CodeKey object {
+		KEY(value from selected code channel): VALUE(number to offset rotXX code)
+	}
+*/
 var CodeKey = {
 	a: 12,
 	b: 14,
@@ -8,6 +14,11 @@ var codeChannel;
 var shiftValue;
 var inputString = '';
 
+/*
+	Every time key released in textinput area, resulting string is:
+		1) converted to upper case (to simplify ASCII codes)
+		2) passed to encode/decode functions based on radio button selection
+*/
 document.getElementById("input").onkeyup = function(){
 	inputString = document.getElementById("input").value.toUpperCase();	  
   if(document.getElementById('mode_encode').checked) {
@@ -17,6 +28,19 @@ document.getElementById("input").onkeyup = function(){
   }
 };
 
+/*
+	Encode and decode functions check code channel and determine offset value from
+	CodeKey object. Then functions iterate through passed string finding utf code
+	for each character. For characters A-Z, utf codes are shifted by value
+	of offset - other character codes passed through as is. All codes pushed
+	to resulting array encodedValues/decodedValues. 
+
+	Encode adds shift value to utf code
+	Decode subtracts shift value from utf codes
+
+	resulting array passed to updateDisplay function
+
+*/
 function encode(str) { // ENCODER FUNCTION
   var encodedValues = [];
   var letterCode;
@@ -31,6 +55,8 @@ function encode(str) { // ENCODER FUNCTION
       encodedValues.push(letterCode);
     } else if (letterCode > 64 && letterCode < 91) {
 	      shiftedLetterCode = letterCode + shiftValue;
+	    		//conditional checks if shifted utf code is beyond A-Z range, 
+	    		//then corrects it by offsetting from 64, right before A(65)
 	    		if(shiftedLetterCode > 90) {
 						shiftedLetterCode = 64 + (shiftedLetterCode - 90);
 					}
@@ -56,6 +82,8 @@ function decode(str) { // DECODER FUNCTION
       decodedValues.push(letterCode);
     } else if (letterCode > 64 && letterCode < 91) {
 	      shiftedLetterCode = letterCode - shiftValue;
+	    		//conditional checks if shifted utf code is below A-Z range, 
+	    		//then corrects it by offsetting from 90(Z)
 	    		if(shiftedLetterCode < 65) {
 						shiftedLetterCode = 90 - (64 - shiftedLetterCode);
 					}
@@ -67,20 +95,27 @@ function decode(str) { // DECODER FUNCTION
   return(updateDisplay(decodedValues));
 }
 
-	//iterates through value of true utf codes - concatenates onto string  
+/*
+	Function iterates through value of true utf codes - concatenates onto string.
+	Logs string for accuracy
+	Places string into DOM in output div
+	Updates href of twitter link with string  
+*/
 function updateDisplay(utfArray) { 
 	var outputString = '';
-	var tweetData;
   for(j=0; j<utfArray.length; j++) {
     outputString = outputString + String.fromCharCode(utfArray[j]);
   }  
   console.log(outputString);
   document.getElementById("output").innerHTML = outputString;
-  document.getElementById("tweet").setAttribute("href", 'https://twitter.com/intent/tweet?text=' + outputString);  
+  document.getElementById("tweet").setAttribute(
+  	"href", 'https://twitter.com/intent/tweet?text=' + outputString);  
 }
 
 
-/* ORIGINAL FUNCTION	  
+/* ---- inspiration for this project ----
+	ORIGINAL FUNCTION - written for freeCodeCamp.com exercise
+	
 
 for(i=0; i<str.length; i++){
 	    letterCode = str.charCodeAt(i);
